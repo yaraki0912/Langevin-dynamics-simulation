c4482566956c1f51573e8f93414ae4b0d1c31a7c
yaraki <yaraki@u.rochester.edu>
2018-10-02 16:44:15 -0400

    commit 1

--- Project_1/Project_1.py
+++ Project_1/Project_1.py
@@ -1,3 +1,177 @@
 # -*- coding: utf-8 -*-
+import argparse
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+
+
+
+def input():
+    #input parameters using argparse
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--temperature', metavar='temperature', type=float, nargs=1,
+                    help=' ')
+    parser.add_argument('--total_time', metavar='total_time', type=float, nargs=1,
+                    help=' ')
+    parser.add_argument('--time_step', metavar='time_step', type=float, nargs=1,
+                    help=' ')
+    parser.add_argument('--initial_position', metavar='initial_position', type=float, nargs=1,
+                    help=' ')
+    parser.add_argument('--initial_velocity', metavar='initial_velocity', type=float, nargs=1,
+                    help=' ')
+    parser.add_argument('--damping_coefficient', metavar='damping_coefficient', type=float, nargs=1,
+                    help=' ')
+
+    return parser
+
+def random_f(temp, damping_coef,K_B=1,epsi=1):
+    var = 2 * temp * damping_coef * K_B * epsi
+    std = np.sqrt(var)  # standard deviation is sqrt of variance
+    random_force=float(np.random.normal(0.0, std))
+    #print("rf", random_force)
+    return random_force
+
+def drag_f(damping_coef,i_velocity):
+    drag_force = -damping_coef * i_velocity
+    #print("df",drag_force)
+    return drag_force
+
+def euler(time_step,velocity,position, damping_coef, temp):
+    #velocity = list()
+    #position = list()
+    #time = list()
+    #n = int(total_time // time_step)
+
+    #velocity.append(i_velocity)
+    #position.append(i_position)
+    #time.append(0.0)
+
+    #for i in range(n):
+    a = drag_f(damping_coef, velocity) + random_f(temp, damping_coef,K_B=1,epsi=1)
+    new_velocity = velocity + a* time_step
+    new_position = position + velocity * time_step
+
+
+        #t = time + time_step
+        #velocity.append(velocity_t)
+        #position.append(position_t)
+        #time.append(t)
+    #print("a,v,p=", a,new_velocity,new_position)
+    return a,new_velocity, new_position
+
+def write_output(output):
+    #Write the output file containing index, time, position, and velocity of particles
+    #assert((len(idx) == len(time)) == (len(position) == len(velocity)))
+    header = "#Index    Time      Position      Velocity"
+    Output = open('Output.txt', "w")
+    Output.write(header)
+    Output.write("\n")
+    for line in output:
+        for i in range(len(line)):
+            if (i == 0):
+                Output.write('{} '.format(line[i]))
+            else:
+                Output.write('{:.4f} '.format(line[i]))
+        Output.write('\n')
+    Output.close()
+
+#def hit_wall(time_step, new_velocity, new_position, damping_coef, temp, time,wall):
+
+
+
+        # await asyncio.sleep(0.05)
+        #write_output(output)
+
+ #           return True
+  #      return False
+
+def histogram():
+    i_position = 0
+    i_velocity = 0
+    time_step = 0.1
+    temp = 300
+    wall = 5
+    total_time = 1000
+    damping_coef = 0.1
+    n = int(total_time // time_step)
+    new_position = i_position
+    new_velocity = i_velocity
+   # time = 0
+    num_took = list()
+    put=[]
+    for i in range(n):
+        if i !=0:
+            new_position = new_pos
+            new_velocity = new_vel
+        for j in range(0,100):
+            new_acc, new_vel, new_pos = euler(time_step, new_velocity, new_position, damping_coef, temp)
+            #time += time_step
+            # index += 1
+            # output.append([index, time, new_pos, new_vel])
+
+            if new_pos >= wall or new_pos <= -wall:
+                num_took.append(j+1)
+                break
+        write_output(put)
+
+    print(num_took, len(num_took))
+
+               #result = hit_wall(time_step, new_velocity, new_position, damping_coef, temp,n, time,wall)
+            #if result== True:
+
+             #   break
+
+    plt.figure()
+    plt.xlabel('Time')
+    plt.ylabel('# of time took to hit wall')
+    #plt.hist(hit_wall())
+    hist_path = os.path.join('histogram.png')
+    plt.savefig(hist_path)
+
+
+
+def main():  # pragma: no cover
+    parser = input()
+    args = parser.parse_args()
+    temp = args.temperature[0]
+    total_time = args.total_time[0]
+    time_step = args.time_step[0]
+    i_position = args.initial_position[0]
+    i_velocity = args.initial_velocity[0]
+    damping_coef = args.damping_coefficient[0]
+    wall=5
+
+    if (damping_coef <= 0 or temp <= 0 or time_step <= 0 or total_time <= 0):
+        # damping coefficient, temperature and time must be positive
+        raise ValueError("Damping coefficient, temperature, time_step and total_time must be non-zero positive values")
+
+    n = int(total_time // time_step)
+    new_position = i_position
+    new_velocity=i_velocity
+    time=0
+    index=0
+    output=[]
+
+    for i in range(n):
+
+        new_acc, new_vel, new_pos = euler(time_step,new_velocity,new_position, damping_coef, temp,)
+        time += time_step
+        index += 1
+        output.append([index, time, new_pos, new_vel])
+        new_position = new_pos
+        new_velocity = new_vel
+        #await asyncio.sleep(0.05)
+        write_output(output)
+        if new_position > wall or new_position < -wall:
+            break
+    return None
+
+if __name__ == '__main__':
+    input()
+    main()
+    histogram()
 
 """Main module."""
+
--- tests/test_Project_1.py
+++ tests/test_Project_1.py
@@ -9,6 +9,12 @@ from click.testing import CliRunner
 
 from Project_1 import Project_1
 from Project_1 import cli
+import io
+import os
+import Lans
+import numpy as np
+import argparse
+import unittest
 
 
 @pytest.fixture
@@ -36,3 +42,61 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_read_energy_read(self):
+        '''Tests if the function reads and returns correctly for given input file'''
+        pos, force, energy = Lans.ReadEnergy(r"./tests/testfile.txt")
+        assert (np.isclose(pos, [0.0001, 0.010099, 0.020099, 0.030098])).all()
+        assert (np.isclose(force, [0.010675, 0.010675, 0.010675, 0.012945])).all()
+        assert (np.isclose(energy, [-0.113475, -0.113475, -0.113475, -0.060178])).all()
+
+    def test_inputs(self):
+        # Tests if the parser reads correctly and if no inputs are given, uses default values
+        self.parser = Lans.CreateParser()
+        parsed = self.parser.parse_args(
+            ['--initial_velocity', '2.5', '--total_time', '20', '--damping_coeff', '0.1', '--input_file',
+             'C:/Users/hetag/Desktop/input.txt'])
+        self.assertEqual(
+            [parsed.initial_position, parsed.initial_velocity, parsed.time_step, parsed.total_time, parsed.temperature,
+             parsed.damping_coeff, parsed.input_file], [1, 2.5, 0.1, 20, 1, 0.1, 'C:/Users/hetag/Desktop/input.txt'])
+
+    kb = 1
+
+    def test_random(self):
+        '''Tests if random generator works fine'''
+        self.assertEqual(Lans.Random(1, 0), 0)
+
+    def test_potential_force(self):
+        '''Tests the function producing potential force'''
+        sample_pos = [0, 1, 2, 3, 4, 5]
+        sample_energy = [0, 10, 20, 30, 40, 50]
+        self.assertEqual(Lans.PotentialForce(2.5, sample_pos, sample_energy), 25)
+
+    def test_drag_force(self):
+        self.assertEqual(Lans.DragForce(0.5, 2), -1)
+
+    def test_euler(self):
+        '''Tests if the force, position and velocity calculated are correct'''
+        self.pos = 1
+        self.vel = 1
+        data = [[1, 2, 3, 4], [10, 20, 30, 40]]
+        self.assertEqual(Lans.Euler(self.pos, self.vel, 0, 1, *data, dt=0.5), (-10, -4, -1))
+
+    def test_write_output(self):
+        '''Tests if the output file is written correctly'''
+        test_string = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+        test_string2 = '1 2.0000 3.0000 4.0000 \n'
+        test_string3 = '5 6.0000 7.0000 8.0000 \n'
+        out_file = './tests/test_output.txt'
+        Lans.write_output(out_file, test_string)
+        f = open(out_file, 'r')
+        test_data = list(f.readlines())
+        f.close()
+        self.assertEqual(test_data[1], test_string2)
+        self.assertEqual(test_data[2], test_string3)

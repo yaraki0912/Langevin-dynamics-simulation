0390e7c790b40144d4b228cf395dcbe6044a6b6a
yaraki0912 <42810842+yaraki0912@users.noreply.github.com>
2018-10-03 10:58:28 -0400

    fixed miss spell

--- tests/test_Project_1.py
+++ tests/test_Project_1.py
@@ -45,7 +45,7 @@ class Test_Project_1(unittest.TestCase):
         
       
     def test_hit_wall(self):
-        self.assertEqual(ptoject_1.hit_wall(0,0,0,0,0,0,0),(0,0,[]))
+        self.assertEqual(project_1.hit_wall(0,0,0,0,0,0,0),(0,0,[]))
     
    # def test_main(self):
     #    self.assertEqual(project_1.main(), None)

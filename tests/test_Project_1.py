5eb2037282d8e0b22f0aa0b17f9ab8fee806847a
yaraki0912 <42810842+yaraki0912@users.noreply.github.com>
2018-10-02 22:33:34 -0400

    output

--- tests/test_Project_1.py
+++ tests/test_Project_1.py
@@ -45,9 +45,9 @@ class Test_Project_1(unittest.TestCase):
 
     def test_write_output(self):
         outfile = StringIO()
-        project_1.output([0.0,0.0,0.0,0.0])
+        project_1.write_output([0.0,0.0,0.0,0.0])
         outfile.seek(0)
         content = outfile.read()
         output_list = content.split('\n')
-        self.assertEqual(project_1.output([0.0,0.0,0.0,0.0]),output_list)
+        self.assertEqual(project_1.write_output([0.0,0.0,0.0,0.0]),output_list)
             

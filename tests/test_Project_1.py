7ccbd1a42dc384f32e48ee4882f035272f83338a
yaraki0912 <42810842+yaraki0912@users.noreply.github.com>
2018-10-02 22:17:49 -0400

    0.0

--- tests/test_Project_1.py
+++ tests/test_Project_1.py
@@ -25,17 +25,9 @@ class Test_Project_1(unittest.TestCase):
 
     
     def test_inputs(self):
-       #parse = project_1.input(['--temperature', '300','--total_time', '1000','--time_step', '0.1','--initial_position', '0','--initial_velocity', '0','--damping_coefficient', '0.1' ])
-
-       #self.assertIsInstance(parse, dict)
-       #self.assertEqual(parse['temperature'], 300)
-       #self.assertEqual(parse['total_time'], 1000)
-       #self.assertEqual(parse['time_step'], 0.1)
-       #self.assertEqual(parse['initial_position'], 0)
-       #self.assertEqual(parse['initial_velocity'], 0)
-       #self.assertEqual(parse['damping_coefficient'], 0.1)
+       
        self.parser = project_1.input()
-       parsed = self.parser.parse_args(['--temperature', '300','--total_time', '1000','--time_step', '0.1','--initial_position', '0','--initial_velocity', '0','--damping_coefficient', '0.1'])
+       parsed = self.parser.parse_args(['--temperature', '300.0','--total_time', '1000.0','--time_step', '0.1','--initial_position', '0.0','--initial_velocity', '0.0','--damping_coefficient', '0.1'])
        self.assertEqual([parsed.temperature,parsed.total_time, parsed.time_step, parsed.initial_position, parsed.initial_velocity,   parsed.damping_coefficient], [300, 1000, 0.1, 0, 0, 0.1] )
  
         
@@ -52,14 +44,10 @@ class Test_Project_1(unittest.TestCase):
         self.assertEqual(project_1.euler(0,0,0,0,0), (0,0))
 
     def test_write_output(self):
-        '''Tests if the output file is written correctly'''
-        test_string = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-        test_string2 = '1 2.0000 3.0000 4.0000 \n'
-        test_string3 = '5 6.0000 7.0000 8.0000 \n' 
-        out_file = './tests/test_output.txt'
-        project_1.write_output(out_file, test_string)
-        output = open(out_file, 'r')
-        test_data = list(output.readlines())
-        output.close()
-        self.assertEqual(test_data[1], test_string2)
-        self.assertEqual(test_data[2], test_string3)
+        outfile = StringIO()
+        simulator.output_file([0,0,0,0], outfile)
+        outfile.seek(0)
+        content = outfile.read()
+        output_list = content.split('\n')
+        self.assertEqual(line, '{0} {1:.2f} {2:.6f} {3:.6f}'.format(0,0,0,0))
+            
